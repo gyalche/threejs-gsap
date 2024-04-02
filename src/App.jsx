aa94deeefba3d442e24e2f1ce6a1996a2e977876
dawa sherpa <sherpadawag1998@gmail.com>
2024-04-02 17:05:51 +0545

    setup completed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="p-10 text-blue-500 text-3xl font-bold underline">
+        HELLO WORLD
+      </h1>
+    </div>
+  );
+};
+
+export default App;
